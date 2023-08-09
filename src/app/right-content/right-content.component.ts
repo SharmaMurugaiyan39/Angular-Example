2116d38ec80295a7a1da39f6abc89b33ff83e148
SharmaMurugaiyan39 <124775198+SharmaMurugaiyan39@users.noreply.github.com>
2023-08-09 13:26:50 +0530

    new code
    sdd

--- src/app/right-content/right-content.component.ts
+++ src/app/right-content/right-content.component.ts
@@ -2,6 +2,7 @@ import { GridPlan } from './../left-menu/left-menu.component';
 import { Component, OnInit, Input } from '@angular/core';
 import { ApiService } from '../app.service';
 import { ToastrService } from 'ngx-toastr'; // Import ngx-toastr service
+import { find, findIndex } from 'rxjs';
 
 declare var bootstrap: any;
 @Component({
@@ -11,13 +12,13 @@ declare var bootstrap: any;
 })
 
 export class RightContentComponent implements OnInit {
-  
+
   tableData: any[] = [];
 
   newType: any;
   isDivHiddenRunning = true;
   isDivHiddenNew = true;
-  isNew :boolean = false;
+  isNew: boolean = false;
 
 
   buttonClick(index: number, item: any) {
@@ -25,96 +26,96 @@ export class RightContentComponent implements OnInit {
 
   }
 
-  openModelAsmCalculation(item : any,i:number): void {
-    this.showTableData=item;
-    console.log(this.showTableData,"show Table")
+  openModelAsmCalculation(item: any, i: number): void {
+    this.showTableData = item;
+    console.log(this.showTableData, "show Table")
     const myModal = document.getElementById('myModalLabelAsm');
     const modal = new bootstrap.Modal(myModal);
-    console.log(item,"",i,"","ASN")
+    console.log(item, "", i, "", "ASN")
     modal.show();
   }
-  openModelRsmCalculation(item : any,i:number): void {
+  openModelRsmCalculation(item: any, i: number): void {
     const myModal = document.getElementById('myModalLabelRsm');
     const modal = new bootstrap.Modal(myModal);
-    console.log(item,"",i,"","RSM")
+    console.log(item, "", i, "", "RSM")
     modal.show();
   }
-  
-  successModel()
-  {
+
+  successModel() {
     const myModal = document.getElementById('successModel');
     const modal = new bootstrap.Modal(myModal);
     modal.show();
   }
 
-  Qty:any;
-  
-  Newindex :any;
+  Qty: any;
 
-  openModelZonalCalculation(item : any,i:number,value : any):void {
+  Newindex: any;
+
+  openModelZonalCalculation(item: any, i: number, value: any): void {
     const myModal = document.getElementById('myModalLabelZonal');
-    this.showTableData=item;
+    this.showTableData = item;
+  
     console.log(this.showTableData);
-    this.tableData[i].apZonalQty=value;
-    for(let j=0;j<this.tableData[i].apZonalQtyList.length;j++)
-    {
-      if(j==this.tableData[i].apZonalQtyList.length-1)
-      {
-      this.tableData[i].apZonalQtyList[j].qty=value;
-      }
-      else{
-        this.tableData[i].apZonalQtyList[j].qty=value;
-      }
-    }
+    this.tableData[i].apZonalQty = value;
+    this.calculationQty(i,value);
     const modal = new bootstrap.Modal(myModal);
-    
-
-    this.Newindex=i;
+    this.Newindex = i;
     modal.show(item);
   }
-  
-  getCalculationZonal(event:any,gridplanId : any)
+
+  calculationQty(i:number ,value : any)
   {
-    const inputValue = event.target.value;
-    console.log(inputValue,388783434,this.Newindex);
-    
-    
-    let i=this.Newindex;
-    this.tableData[i].apZonalQty=inputValue;
-    for(let j=0;j<this.tableData[i].apZonalQtyList.length;j++)
-     {
-       if(j==this.tableData[i].apZonalQtyList.length-1)
-       {
-       this.tableData[i].apZonalQtyList[j].qty=inputValue;
-       }
-       else{
-         this.tableData[i].apZonalQtyList[j].qty=inputValue;
-       }
-     }
+    let number=0;
+    let totalZonal = value / 2;
+    this.tableData[i].apZonalQty = value;
+    for (let j = 0; j < this.tableData[i].apZonalQtyList.length; j++) {
+      if (totalZonal.toString().includes('.')) {
+        let dotSplit = totalZonal.toString().split('.');
+        console.log("log", dotSplit[1])
+        number = number + parseFloat('.' + dotSplit[1]);
+        this.tableData[i].apZonalQtyList[j].qty = dotSplit[0];
+      }
+      else {
+        this.tableData[i].apZonalQtyList[j].qty = totalZonal;
+
+      }
+      if (j == this.tableData[i].apZonalQtyList.length - 1) {
+        let dotSplit = totalZonal.toString().split('.');
+        this.tableData[i].apZonalQtyList[j].qty = parseInt(dotSplit[0]) + number;
+      }
+    }
   }
 
-  
-  
-  onSavedZonalQtyCalculation()
-  {
-    let i=this.Newindex;
+  getCalculationZonal(event: any, gridplanId: any) {
+    const inputValue = event.target.value;
+    console.log(inputValue, 388783434, this.Newindex);
+    let i = this.Newindex;
+    this.calculationQty(i,inputValue);
   }
-  showTableData:any =[];
+
+
+  totalQty: number = 0;
+
+  onSavedZonalQtyCalculation() {
+    let i = this.Newindex;
+  }
+  showTableData: any = [];
   formSubmitted: boolean = false;
   successMessage: string = 'Form submitted successfully!';
   isLoading: boolean = true;
   showDiv: boolean = false;
   tapClickEnableRunning(type: String, selectedItem: any) {
 
-    this.tableData=[];
+    this.tableData = [];
+    this.NewtableData = [];
     this.tap.forEach(button => (button.isActive = false));
     selectedItem.isActive = true;
 
-   
+
 
     this.newType = selectedItem.label;
 
-    let flag=false;
+    let flag = false;
 
     this.apiService.currentContent.subscribe((content) => {
       let id = content;
@@ -122,9 +123,20 @@ export class RightContentComponent implements OnInit {
         (data) => {
           this.tableData = data.body;
           this.isNew = true;
-          flag=true;
+          flag = true;
+          let number = 0;
+          for (let i = 0; i < this.tableData.length; i++) {
+
+
+            if (this.tableData[i].apZonalQty !== null) {
+              number = number + parseInt(this.tableData[i].apZonalQty);
+            }
+          }
+          this.totalQty = number;
+          console.log(number, "total");
+          console.log(this.totalQty, "qty");
           this.isLoading = false;
-          
+
         },
         (error) => {
           console.error('Error fetching data:', error);
@@ -137,10 +149,11 @@ export class RightContentComponent implements OnInit {
     if (length === 0) {
       this.showDiv = false;
     }
-    else { 
+    else {
       this.showDiv = true;
     }
-    
+
+
     this.isDivHiddenRunning = !this.isDivHiddenRunning;
   }
   tapClickEnableNew(type: String) {
@@ -163,18 +176,27 @@ export class RightContentComponent implements OnInit {
   }
   selectedItem!: string;
 
-  constructor(private apiService: ApiService,private toastr: ToastrService) { }
+  constructor(private apiService: ApiService, private toastr: ToastrService) { }
 
   @Input() clkEve: any;
 
   fetchTableData() {
 
+  }
 
+  getDimensionsByFilter(event: any) {
+    return this.tableData.filter(x => x.brand === event.target.value);
   }
 
   ngOnInit() {
-    
-   this.tapClickEnableRunning("running", this.tap[0])
+
+    this.clkEve = true;
+    console.log("click evt", this.clkEve);
+    this.totalQty = 0;
+    setTimeout(() => {
+      this.tapClickEnableRunning("running", this.tap[0])
+    }, 800);
+
   }
 
   tap = [
@@ -182,30 +204,66 @@ export class RightContentComponent implements OnInit {
     { label: 'New', isActive: false }
   ];
   showMessage: boolean = false;
-  
-  saveFunction()
-  {
-    this.apiService.saveZonalPostRequest(this.tableData,this.tableData[0].gridPlanId).subscribe(
+
+
+  NewtableData: any[] = [];
+
+  filterFlag: boolean = false;
+
+  keyPressedFilter(findStr: any, flag: number, searchColumn: string) {
+
+    let number: number = 0;
+    console.log("Search column", searchColumn);
+
+    if (flag == 1) {
+      const filteredData = this.NewtableData.filter(item => item[searchColumn].toString().toLowerCase().includes(findStr.toLowerCase()));
+      this.NewtableData = filteredData;
+      this.filterFlag = true;
+      for (let i = 0; i < this.NewtableData.length; i++) {
+
+        number = number + parseInt(this.NewtableData[i].apZonalQty);
+      }
+
+
+    }
+    else if (flag == 2) {
+      this.filterFlag = true;
+      const filteredData = this.tableData.filter(item => item[searchColumn].toString().toLowerCase().includes(findStr.toLowerCase()));
+      this.NewtableData = filteredData;
+      for (let i = 0; i < this.NewtableData.length; i++) {
+
+        number = number + parseInt(this.NewtableData[i].apZonalQty);
+      }
+    }
+    else {
+      this.filterFlag = false;
+    }
+
+    this.totalQty = number;
+  }
+
+
+  saveFunction() {
+    this.apiService.saveZonalPostRequest(this.tableData, this.tableData[0].gridPlanId).subscribe(
       (response) => {
         console.log('Response from the API:', this.tableData[0].gridPlanId);
         this.toastr.success('Zonal Grid Plan Save Successfully!', 'Success');
-       // this.successModel();
+        // this.successModel();
 
         // Handle the API response here
-   
+
       },
       (error) => {
         console.error('Error while making the API call:', error);
         // Handle errors here
       }
     );
-    
+
 
   }
-  confirmFunction()
-  {
+  confirmFunction() {
 
   }
- 
+
 
 }
