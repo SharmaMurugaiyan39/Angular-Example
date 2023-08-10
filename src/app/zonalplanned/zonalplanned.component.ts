29883937a37703905db41878c65582f7df023d59
SharmaMurugaiyan39 <124775198+SharmaMurugaiyan39@users.noreply.github.com>
2023-08-10 11:20:57 +0530

    net

--- src/app/zonalplanned/zonalplanned.component.ts
+++ src/app/zonalplanned/zonalplanned.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, OnInit,EventEmitter } from '@angular/core';
+import { Component, Output, OnInit, EventEmitter } from '@angular/core';
 import { ApiService } from '../app.service';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
@@ -11,46 +11,56 @@ import { Location } from '@angular/common';
 export class ZonalplannedComponent implements OnInit {
 
   zonalPlanned = [
-    { label: 'Yet to Allocated', isActive: false , value:"pending"},
-    { label: 'Completed', isActive: false,value:"planned"}
+    { label: 'Yet to Allocated', isActive: false, value: "pending" },
+    { label: 'Completed', isActive: false, value: "planned" }
   ];
 
-  isNew:boolean=false;
-  
-  zonalRsmData :any[] = [];
-  
-  
-  @Output() statePlanId:any =new EventEmitter();
-  constructor(private apiService: ApiService,private router: Router) { }
+  isNew: boolean = false;
+
+  zonalRsmData: any[] = [];
+
+
+  @Output() statePlanId: any = new EventEmitter();
+  constructor(private apiService: ApiService, private router: Router) { }
 
   ngOnInit(): void {
-   this.zonalPlannedClick("pending", this.zonalPlanned[0],0);
+    this.zonalPlannedClick("pending", this.zonalPlanned[0], 0);
 
-     this.zonalPlanned[0].isActive=true;
-  
+    this.zonalPlanned[0].isActive = true;
+    this.runningFlag=false;
+
 
   }
 
 
-  
-  planned : boolean =false;
-  pending : boolean =false;
+
+  planned: boolean = false;
+  pending: boolean = false;
 
   showDiv: boolean = false;
-  zonalPlannedClick(type: String, selectedItem: any,i:number) {
+  runningFlag: boolean = false;
+  zonalPlannedClick(type: String, selectedItem: any, i: number) {
+
     this.zonalPlanned.forEach(button => (button.isActive = false));
-    
-    this.zonalPlanned[i].isActive=true;
+
+    this.zonalPlanned[i].isActive = true;
     this.apiService.currentContent.subscribe((content) => {
       let id = content;
       this.apiService.getZonalPlanned(selectedItem.value).subscribe(
         (data) => {
-          this.zonalRsmData=data.body;
-         console.log(this.zonalRsmData);
-         this.isNew=true;
+          this.zonalRsmData = data.body;
+          console.log(this.zonalRsmData);
+          this.isNew = true;
+          console.log(selectedItem.value, "tap")
+          if (selectedItem.value !== 'planned') {
+            this.runningFlag = true;
+          }
+          else
+          {
+            this.runningFlag = false;
+          }
 
-       
-         
+
         },
         (error) => {
           console.error('Error fetching data:', error);
@@ -59,16 +69,14 @@ export class ZonalplannedComponent implements OnInit {
     });
   }
 
-  
-  clickEventOfGridPlan(statePlanId : any,type:string)
-  {
 
-    if(type==='confirm')
-    {
+  clickEventOfGridPlan(statePlanId: any, type: string) {
+
+    if (type === 'confirm') {
       this.apiService.confirmZonalPostRequest(statePlanId).subscribe(
         (response) => {
           console.log('Response from the API:', statePlanId);
-  
+
           // Handle the API response here
         },
         (error) => {
@@ -78,21 +86,20 @@ export class ZonalplannedComponent implements OnInit {
       );
 
     }
-    else
-    {
-    this.router.navigate(['/zonal-detail',statePlanId]);
+    else {
+      this.router.navigate(['/zonal-detail', statePlanId]);
     }
-    
-    
-    
-    
-    
-  
+
+
+
+
+
+
   }
 
-  
 
 
 
-  
+
+
 }
